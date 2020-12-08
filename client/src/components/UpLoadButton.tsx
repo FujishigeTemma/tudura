ebdd3ad7fa91adb888a4cda085a567ebe985af6f
hijiki51 <hibiki0719euph@gmail.com>
2020-12-08 22:56:04 +0900

    :shirt: add type

--- client/src/components/UpLoadButton.tsx
+++ client/src/components/UpLoadButton.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import Color from '../style/Color'
 
-const buildFileSelector = () => {
+const buildFileSelector = (): HTMLInputElement => {
   const fileSelector: HTMLInputElement = document.createElement('input');
   fileSelector.setAttribute('type', 'file');
   fileSelector.setAttribute('multiple', 'multiple');
@@ -22,7 +22,7 @@ const UpLoadButton: React.FC = () => {
   }
 
   return (
-    <Button type='button' onClick={(event: React.MouseEvent<HTMLElement, MouseEvent>) => { handleFileSelect(event) }}>
+    <Button onClick={(event: React.MouseEvent<HTMLElement, MouseEvent>) => handleFileSelect(event)}>
       <UploadIcon xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'>
         <path d='M9 16h6v-6h4l-7-7-7 7h4zm-4 2h14v2H5z' />
       </UploadIcon>
