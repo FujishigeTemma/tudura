6c2684751541ac40c599980a1cb634b732033bd4
hijiki51 <hibiki0719euph@gmail.com>
2020-12-11 15:47:23 +0900

    :art: fix newbox modal

--- client/src/components/NewBoxModal.tsx
+++ client/src/components/NewBoxModal.tsx
@@ -10,6 +10,42 @@ import Color from '../style/Color'
 // }
 
 
+
+//TODO:名前変える
+const NewBoxModal: React.FC = () => {
+  return (
+    <Modal ariaHideApp={false} isOpen={true} style={ModalStyle}>
+      <ModalForm>
+        <ModalTitle>Create New Box </ModalTitle>
+        <BoxName>
+          <ParamTitle>Name</ParamTitle>
+          <BoxNameInputForm
+            type='text'
+            name='name'
+            placeholder='Enter BoxName'
+            autoFocus
+          />
+        </BoxName>
+        <PasswordForm>
+          <ParamTitle>Password</ParamTitle>
+          <PasswordCheckBox></PasswordCheckBox>
+        </PasswordForm>
+        <PasswordInputForm
+          type='password'
+          name='password'
+          placeholder='Enter password'
+          autoFocus
+        />
+        <Link to='/'>
+          <Cancel type='button'>cancel</Cancel>
+        </Link>
+        <Create type='submit'>submit</Create>
+      </ModalForm>
+    </Modal>
+  )
+
+}
+
 const ModalStyle: Modal.Styles = {
   content: {
     top: '50%',
@@ -26,67 +62,33 @@ const ModalStyle: Modal.Styles = {
   }
 }
 
-//TODO:名前変える
-const NewBoxModal: React.FC = () => {
-  return (
-    <Modal ariaHideApp={false} isOpen={true} style={ModalStyle}>
-      <ModalForm>
-        <ModalTitle>Create New Box </ModalTitle>
-        <BoxName>
-          <ParamTitle>Name</ParamTitle>
-          <BoxNameForm
-            type='text'
-            name='name'
-            placeholder='Enter BoxName'
-            autoFocus
-          />
-        </BoxName>
-        <PasswordForm>
-          <ParamTitle>Password</ParamTitle>
-          <PasswordCheckBox></PasswordCheckBox>
-          <PasswordInputForm
-            type='password'
-            name='password'
-            placeholder='Enter password'
-            autoFocus
-          />
-        </PasswordForm>
-        <Link to='/'>
-          <Cancel type='button'>cancel</Cancel>
-        </Link>
-        <Create type='submit'>submit</Create>
-      </ModalForm>
-    </Modal>
-  )
-
-}
-
-
-
-
 const ModalForm = styled.form`
   font-size: min(3.2vw, 1rem);
-  padding: 7em 3em;
+  padding: 2em 3em;
 `
 
 
 const ModalTitle = styled.div`
-  font-size: 1.5em;
-  margin: 0 0 2em;
+  font-size: 2em;
+  margin: 0 -1em 2em;
 `
 
 const ParamTitle = styled.div`
+  line-height:1.5em;
+  margin: 0 1em 0 0;
   font-size: 1.5em;
 `
 
 const BoxName = styled.div`
-  display:flex;
-  justify-content:center;
-`
-const BoxNameForm = styled.input`
   display: flex;
+  justify-content: center;
+  margin:0 0 1rem 0;
+`
+const BoxNameInputForm = styled.input`
+  display: flex;
+  width : 19.7rem;
   font-size: 1.5em;
-  padding: 0.5em;
+  padding: 0.3em;
   margin: 0 1em;
   border-radius: 0.5em;
   outline: none;
@@ -102,15 +104,18 @@ const BoxNameForm = styled.input`
 `
 
 const PasswordForm = styled.div`
-  dixplay:flex;
-  float :center;
+  display: flex;
+  float: center;
+  margin:  0 0 1em 0;
 `
 
 const PasswordInputForm = styled.input`
   display: flex;
+  width: 26rem;
+  height:1.5rem;
   font-size: 1.5em;
   padding: 0.5em;
-  margin: 0 1em;
+  margin: 0 0;
   border-radius: 0.5em;
   outline: none;
   border: 2px solid ${Color.SECONDARY_SUB};
@@ -123,7 +128,11 @@ const PasswordInputForm = styled.input`
     color: #C4C4C4;
   }
 `
-const PasswordCheckBox = styled.input.attrs({ type: 'checkbox' })``
+const PasswordCheckBox = styled.input.attrs({ type: 'checkbox' })`
+  margin: 1em 0 0 0 ;
+  transform:scale(2.0);
+  flex-wrap:wrap
+`
 
 const Create = styled.button`
   font-size: 1.4em;
