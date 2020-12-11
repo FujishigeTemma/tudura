ae667096808788b54209d6560884fecd491a39e3
trasta298 <trasta298@gmail.com>
2020-12-12 02:45:49 +0900

    :sparkles: GET /boxes/boxid 実装

--- client/src/components/Box.tsx
+++ client/src/components/Box.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import styled from 'styled-components'
 import { useParams } from 'react-router-dom'
 import BoxContents from './BoxContents'
@@ -9,21 +10,53 @@ interface BoxParams {
   boxid: string
 }
 
+interface Item {
+  id: string
+  name: string
+  expirationDate: Date
+}
+
+interface GetBoxesResponse {
+  id: string
+  name: string
+  passwordRequired: boolean
+  items: Item[]
+  updatedAt: Date
+}
+
 const Box: React.FC = () => {
 
   const { boxid } = useParams<BoxParams>()
   const [ isBox, setIsBox ] = useState(true) // 存在するboxidかどうか
-  const [ isAuth, setIsAuth ] = useState(false) // パスワード認証が必要かどうか
+  const [ isAuth, setIsAuth ] = useState(false) // 認証されているかどうか
 
   useEffect((): void => {
-    setIsAuth(false)
-    if (boxid !== 'a') {
-      setIsBox(false)
-    }
+    setBoxInfo()
   }, [])
 
   const authenticate = (): void => {
     setIsAuth(true)
+  }
+
+  const setBoxInfo = async () => {
+    const boxInfo = await getBoxes()
+    if (boxInfo === null) {
+      setIsBox(false)
+      return
+    }
+    if (boxInfo.passwordRequired === false) {
+      setIsAuth(true)
+    }
+  }
+
+  const getBoxes = async (): Promise<GetBoxesResponse | null> => {
+    try {
+      const res = await axios.get<GetBoxesResponse>(`${process.env.REACT_APP_API_SERVER}/boxes/${boxid}`)
+      return res.data
+    } catch(err) {
+      console.log('error')
+      return null
+    }
   }
 
   return (
