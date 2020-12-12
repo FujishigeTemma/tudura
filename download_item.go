535fe7e7178fb3b862fb9fc0da911e1edfa7f094
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2020-12-13 02:40:03 +0900

    :sparkles:ダウンロード用APIの追加

--- download_item.go
+++ download_item.go
@@ -3,21 +3,18 @@ package function
 import (
 	"context"
 	"database/sql"
-	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
-	"mime"
-	"mime/multipart"
 	"net/http"
-	"strings"
 	"time"
 
 	"cloud.google.com/go/storage"
-	"github.com/google/uuid"
 )
 
+type itemInfo struct {
+	IsExpired bool `db:"is_expired"`
+}
 
 // TODO: 一括アップロード可能に
 // DownloadItemHandler POST /boxes/{boxId}/d/{itemId} アイテムのダウンロード
@@ -44,6 +41,11 @@ func DownloadItemHandler(w http.ResponseWriter, r *http.Request) {
 	// Set CORS headers for the main request.
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
+	// TODO: Boxのauth時にcookieを付与→cookieがない場合はBoxのauthにリダイレクト
+	//if len(boxInfo.HashedPass) != 0 {
+	//
+	//}
+
 	var err error
 	// TODO: コネクションの効率化
 	storageClient, err = storage.NewClient(ctx)
@@ -53,13 +55,13 @@ func DownloadItemHandler(w http.ResponseWriter, r *http.Request) {
 	defer storageClient.Close()
 
 	Trace.Printf("Debug: %v", r.URL.Path)
-	boxID := strings.TrimPrefix(r.URL.Path, "/boxes/")
+	itemID := r.URL.Path[46:]
 
-	var boxInfo boxInfo
-	err = dbPool.Get(&boxInfo, "SELECT hashed_pass FROM boxes WHERE id = ? AND deleted_at IS NULL", boxID)
+	var itemInfo itemInfo
+	err = dbPool.Get(&itemInfo, "SELECT CASE WHEN expires_at <= ? THEN 1 ELSE 0 END AS is_expired FROM items WHERE id = ?", time.Now(), itemID)
 	if err == sql.ErrNoRows {
-		http.Error(w, "Box Not Found", http.StatusNotFound)
-		Info.Printf("box not found: %v", boxID)
+		http.Error(w, "Item Not Found", http.StatusNotFound)
+		Info.Printf("item not found: %v", itemID)
 		return
 	}
 	if err != nil {
@@ -67,92 +69,25 @@ func DownloadItemHandler(w http.ResponseWriter, r *http.Request) {
 		Error.Printf("error occured when SELECT box record: %v", err)
 		return
 	}
+	if itemInfo.IsExpired {
+		http.Error(w, "Resource Is Expired", http.StatusGone)
+		Info.Printf("expired resource access: %v", itemID)
+		return
+	}
 
-	// TODO: Boxのauth時にcookieを付与→cookieがない場合はBoxのauthにリダイレクト
-	//if len(boxInfo.HashedPass) != 0 {
-	//
-	//}
-
-	var req postItemRequest
-
-	mediaType, params, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
+	file, err := downloadFile(bucketName, itemID)
 	if err != nil {
-		http.Error(w, "Invalid Content-Type", http.StatusBadRequest)
-		Error.Printf("mime.ParseMediaType: %v", err)
-		return
-	}
-	if strings.HasPrefix(mediaType, "multipart/") {
-		mr := multipart.NewReader(r.Body, params["boundary"])
-		for {
-			p, err := mr.NextPart()
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				http.Error(w, "Invalid Form Data", http.StatusBadRequest)
-				Error.Printf("mr.NextPart: %v", err)
-				return
-			}
-			Trace.Printf("Dubug: %v", p.Header)
-			if p.FormName() == "json" {
-				if err := json.NewDecoder(p).Decode(&req); err != nil {
-					http.Error(w, "Invalid JSON format", http.StatusBadRequest)
-					Error.Printf("json.NewDecoder: %v", err)
-					return
-				}
-
-				if len(req.Name) <= 0 || len(req.Name) > 32 {
-					http.Error(w, "Item Name must be between 1 and 32 characters.", http.StatusBadRequest)
-					Info.Printf("item name length error: %v", req.Name)
-					return
-				}
-			}
-			if p.FormName() == "file" {
-				if err := uploadFile(p, bucketName, req.Name, req.Duration); err != nil {
-					http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-					Error.Printf("uploadFile(): %v", err)
-					return
-				}
-			}
-		}
-	}
-
-	// transactionを取る
-	itemID, err := uuid.NewRandom()
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		Error.Printf("uuid.NewRandom: %v", err)
-		return
-	}
-	var expirationDate time.Time
-	if req.Duration == 0 {
-		expirationDate = time.Now().Add(time.Minute * 30)
-	} else {
-		expirationDate = time.Now().Add(time.Minute * time.Duration(req.Duration))
-	}
-	if _, err := dbPool.Exec("INSERT INTO items(`id`, `box_id`, `name`, `expires_at`) VALUES (?, ?, ?, ?)", itemID.String(), boxID, req.Name, expirationDate); err != nil {
-		http.Error(w, "DB Error", http.StatusInternalServerError)
-		Error.Printf("error occured when INSERT item record: %v", err)
-	}
-	var uploadedFile uploadedFile
-	if err := dbPool.Get(&uploadedFile, "SELECT id, box_id, name, expires_at FROM items WHERE id = ?", itemID.String()); err != nil {
-		http.Error(w, "DB Error", http.StatusInternalServerError)
-		Error.Printf("error occured when SELECT uploaded item record: %v", err)
+		http.Error(w, "Resource Error", http.StatusInternalServerError)
+		Error.Printf("downloadFile(): %v", err)
 		return
 	}
 
-	res, err := json.Marshal(uploadedFile)
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		Error.Printf("json.Marshal: %v", err)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(res)
+	w.Header().Set("Content-Length", string(len(file)))
+	w.Write(file)
 }
 
 // TODO: bucketをout of factorする
-func downloadFile(bucket, object string) (io.Writer, error) {
+func downloadFile(bucket, object string) ([]byte, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*50)
 	defer cancel()
 
@@ -164,6 +99,7 @@ func downloadFile(bucket, object string) (io.Writer, error) {
 	}
 	defer rc.Close()
 
+	// TODO: streamにしたい
 	data, err := ioutil.ReadAll(rc)
 	if err != nil {
 		return nil, fmt.Errorf("ioutil.ReadAll: %v", err)
