10134df624284a28e132f789614aabd21c63cc3c
trasta298 <trasta298@gmail.com>
2020-12-13 03:22:33 +0900

    :art: card css

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import styled from 'styled-components'
 import SaveFile from './Savefile'//デバッグ用
 import { Item } from './Box'
+import Color from '../style/Color'
 import { ErrorResponse, PostItemResponse } from '../types/Response'
 
 interface ItemProps {
@@ -48,15 +49,49 @@ const Card: React.FC<ItemProps> = ({ boxid, item }: ItemProps) => {
   }
 
   return (
-    <CardDefault onClick={() => saveDownloadFile()}>{item.name}</CardDefault>
+    <CardDefault onClick={() => saveDownloadFile()}>
+      <Thumbnail></Thumbnail>
+      <CardName>
+        <FileName>{item.name}</FileName>
+      </CardName>
+    </CardDefault>
   )
 }
 
+const Thumbnail = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 20%;
+  border-bottom: 2px solid ${Color.PRIMARY_SUB};
+`
+
+const CardName = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 80%;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`
+
+const FileName = styled.div`
+  font-size: 25px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
+
 const CardDefault = styled.button`
-  background: #C4C4C4;
+  border-radius: 5%;
   position: relative;
-  border: none;
+  border: solid 2px ${Color.PRIMARY_SUB};
+  box-shadow: 0.5px 2px 4px 0 rgba(94, 108, 191, 0.4);
   outline: none;
+  box-sizing: content-box;
   &:hover {
     cursor: pointer;
     opacity: 0.8;
