b9eec577d311c3229dd46172be4ff726a83043dc
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2020-12-15 23:59:37 +0900

    :sparkles:add filesize

--- init.sql
+++ init.sql
@@ -13,12 +13,13 @@ CREATE TABLE IF NOT EXISTS items (
     id           VARCHAR(36) NOT NULL PRIMARY KEY,
     box_id       VARCHAR(36) NOT NULL,
     name         TEXT NOT NULL,
+    size         INT NOT NULL,
     expires_at   DATETIME NOT NULL,
     created_at   DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at   DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     INDEX index_box_id (`box_id`),
     CONSTRAINT fk_boxes_id
-        FOREIGN KEY (`box_id`) 
+        FOREIGN KEY (`box_id`)
         REFERENCES boxes (`id`)
         ON DELETE CASCADE
         ON UPDATE CASCADE
